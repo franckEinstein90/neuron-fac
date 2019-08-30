674d0d4738967cfca0ba2478111639d0e909baf3
U-APCA2\Franck Binard <Franck Binard@BNO-L-PC0ANN8B.APCA2.GC.CA>
2019-08-30 16:55:52 -0400

    tests for typeSignature module

--- test/typeSignature.js
+++ test/typeSignature.js
@@ -10,7 +10,7 @@ const expect = require('chai').expect;
 describe("TypeSignature Object", function(){
     it("is created using the TypeSignature constructor", function(){
         let ts = new typeSignatures.TypeSignature([]);
-//        expect(ts).to.be.an.instanceOf('object');
         expect(ts).to.be.an('object')
+        expect(ts).to.be.an.instanceOf('object');
     })
 })
